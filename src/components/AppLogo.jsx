4b0eeac0d2482977fe1ec1fb820599f183e45d75
fabiocatone <fabbiocatone@email.com>
2026-04-27 17:09:28 +0200

    header nav ok (no icons) e main ok

--- src/components/AppLogo.jsx
+++ src/components/AppLogo.jsx
@@ -1,5 +1,5 @@
 import Logo from "../assets/react.svg"
 
-export default function AppLogo () {
-     return (<img src={Logo} alt="Logo" />)
+export default function AppLogo ({size}) {
+     return (<img src={Logo} alt="Logo"  width={size}/>)
 }
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,5 +1,15 @@
-export default function() {
+import AppLogo from "./AppLogo"
+
+export default function AppMain() {
     return (
-        <div></div>
+        <main> 
+<div className="SpaceLogo">
+    <AppLogo size = {75}/>
+    <h1 className="TitleLogo">
+        Welcome to the React Community
+    </h1>
+    <a className="btn">Get Started</a>
+</div>
+        </main>
     )
 }
